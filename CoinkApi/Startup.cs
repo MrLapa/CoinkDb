ca10674ac8f1c55a1dba4922ee4c5a447fde085f
CARLOS GARCIA <mrgarcia7@gmail.com>
2020-08-14 09:24:32 -0500

    registerin user repository in startup

--- CoinkApi/Startup.cs
+++ CoinkApi/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DomainLayer.Interfaces;
+using DomainLayer.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace CoinkApi
 {
@@ -26,6 +21,7 @@ namespace CoinkApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IUserRepository, UserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
