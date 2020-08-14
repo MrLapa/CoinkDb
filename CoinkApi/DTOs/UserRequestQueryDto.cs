f78d57470707c49eae2be4c6da36b219334e27f1
CARLOS GARCIA <mrgarcia7@gmail.com>
2020-08-14 09:23:54 -0500

    creating DTOs for request

---
+++ CoinkApi/DTOs/UserRequestQueryDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoinkApi.DTOs
+{
+    public class UserRequestQueryDto
+    {
+        public int? Id { get; set; }        
+        
+        [StringLength(50, MinimumLength = 0)]
+        public string Name { get; set; }
+        
+        [StringLength(50, MinimumLength = 0)]
+        public string Phone { get; set; }
+
+        [StringLength(50, MinimumLength = 0)]
+        public string Address { get; set; }        
+        public int? CityId { get; set; }
+    }
+}
