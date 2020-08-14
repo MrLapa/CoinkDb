a92eda39d6ac9d9e21664d7791dda97896465823
CARLOS GARCIA <mrgarcia7@gmail.com>
2020-08-14 07:05:06 -0500

    Creating new property for User entity

--- DataLayer/Entities/User.cs
+++ DataLayer/Entities/User.cs
@@ -7,17 +7,19 @@
         public string Phone { get; set; }
         public string Address { get; set; }
         public City City { get; set; }
+        public int CityId { get; set; }
         public User()
         {
 
         }
-        public User(int id, string name, string phone, string address, City city)
+        public User(int id, string name, string phone, string address, City city, int cityId)
         {
             Id = id;
             Name = name;
             Phone = phone;
             Address = address;
             City = city;
+            CityId = cityId;
         }
     }
 }
