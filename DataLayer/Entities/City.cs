170ac7e049f448435f98ae30ff7537b0ebbe9370
CARLOS GARCIA <mrgarcia7@gmail.com>
2020-08-14 04:06:18 -0500

    Creating entities

---
+++ DataLayer/Entities/City.cs
@@ -0,0 +1,19 @@
+﻿namespace DataLayer.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Department Department { get; set; }
+        public City()
+        {
+
+        }
+        public City(int id, string name, Department department)
+        {
+            Id = id;
+            Name = name;
+            Department = department;
+        }
+    }
+}
---
+++ DataLayer/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿namespace DataLayer.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public City City { get; set; }
+        public User()
+        {
+
+        }
+        public User(int id, string name, string phone, string address, City city)
+        {
+            Id = id;
+            Name = name;
+            Phone = phone;
+            Address = address;
+            City = city;
+        }
+    }
+}
